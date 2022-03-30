95fe73bf8a390379bb63384494f532e6be77ef49
Dhiraj Y <dhiraj.y@vserv.com>
2022-03-30 13:17:14 +0530

    test functionl

---
+++ my.js
@@ -0,0 +1,5 @@
+// this is way we can send  and show message 
+alert("this is test");
+console.log("this is test");
+document.write("print in browser");
+document.getElementById('box').innerHTML="Show in Html text";
